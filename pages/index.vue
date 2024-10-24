75d77adba92d4ce1400fdfecacb6b481b3b80982
zahara120 <zaharaa120@gmail.com>
2024-10-24 11:33:19 +0700

    feat: add prettier for tailwind classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <main
-    class="grid grid-cols-1 justify-center min-h-screen md:grid-cols-2 lg:grid-cols-4 bg-[#F6F5F6] gap-8 p-5"
+    class="grid min-h-screen grid-cols-1 justify-center gap-8 bg-[#F6F5F6] p-5 md:grid-cols-2 lg:grid-cols-4"
   >
     <section
       data-aos="fade-right"
       data-aos-duration="1000"
-      class="md:col-span-2 lg:col-span-2 lg:col-start-2 flex flex-col gap-5 items-center justify-center py-8 bg-purple-500 text-center rounded-xl"
+      class="flex flex-col items-center justify-center gap-5 rounded-xl bg-purple-500 py-8 text-center md:col-span-2 lg:col-span-2 lg:col-start-2"
     >
       <h1 class="text-preset-2 text-white">
         Social Media <span class="text-yellow-500">10x</span>
@@ -16,13 +16,13 @@
         alt="illustration-five-stars"
         class="w-1/2"
       />
-      <p class="text-white text-preset-4">Over 4,000 5-star reviews</p>
+      <p class="text-preset-4 text-white">Over 4,000 5-star reviews</p>
     </section>
 
     <section
       data-aos="fade-down"
       data-aos-duration="1000"
-      class="lg:row-start-2 flex flex-col gap-5 items-center justify-center bg-white rounded-xl p-5"
+      class="flex flex-col items-center justify-center gap-5 rounded-xl bg-white p-5 lg:row-start-2"
     >
       <img
         src="/images/illustration-multiple-platforms.webp"
@@ -34,7 +34,7 @@
     <section
       data-aos="fade-down"
       data-aos-duration="1000"
-      class="lg:row-start-2 flex flex-col gap-5 justify-center lg:items-center p-5 bg-yellow-500 rounded-xl pb-0"
+      class="flex flex-col justify-center gap-5 rounded-xl bg-yellow-500 p-5 pb-0 lg:row-start-2 lg:items-center"
     >
       <h1 class="text-preset-3">Maintain a consistent posting schedule.</h1>
       <img
@@ -47,7 +47,7 @@
     <section
       data-aos="fade-right"
       data-aos-duration="1000"
-      class="md:col-span-2 lg:col-span-2 lg:row-span-2 lg:col-start-4 lg:row-start-1 flex flex-col gap-5 lg:items-start items-center justify-center p-5 bg-purple-100 text-center lg:text-start rounded-xl"
+      class="flex flex-col items-center justify-center gap-5 rounded-xl bg-purple-100 p-5 text-center md:col-span-2 lg:col-span-2 lg:col-start-4 lg:row-span-2 lg:row-start-1 lg:items-start lg:text-start"
     >
       <h1 class="text-preset-4">Schedule to social media.</h1>
       <img
@@ -64,14 +64,14 @@
     <section
       data-aos="fade-left"
       data-aos-duration="1000"
-      class="md:col-span-2 lg:col-span-3 lg:row-span-1 flex flex-col md:flex-row md:justify-evenly gap-5 items-center justify-center p-8 bg-purple-500 text-center rounded-xl"
+      class="flex flex-col items-center justify-center gap-5 rounded-xl bg-purple-500 p-8 text-center md:col-span-2 md:flex-row md:justify-evenly lg:col-span-3 lg:row-span-1"
     >
       <img
         src="/images/illustration-grow-followers.webp"
         alt="illustration-schedule-posts"
         class="max-w-[227px]"
       />
-      <h1 class="text-preset-3 lg:text-preset-2 text-white">
+      <h1 class="text-preset-3 text-white lg:text-preset-2">
         Grow followers with non-stop content.
       </h1>
     </section>
@@ -79,7 +79,7 @@
     <section
       data-aos="fade-right"
       data-aos-duration="1000"
-      class="md:col-span-2 lg:col-span-1 lg:row-span-1 lg:col-start-2 lg:row-start-3 flex flex-col gap-5 justify-center p-8 bg-white rounded-xl"
+      class="flex flex-col justify-center gap-5 rounded-xl bg-white p-8 md:col-span-2 lg:col-span-1 lg:col-start-2 lg:row-span-1 lg:row-start-3"
     >
       <h1 class="text-preset-2 lg:text-preset-1">>56%</h1>
       <p class="text-preset-4">faster audience growth</p>
@@ -91,11 +91,11 @@
     </section>
 
     <section
-      class="lg:row-span-2 lg:col-start-1 lg:row-start-1 flex flex-col gap-5 justify-center p-8 bg-yellow-100 rounded-xl"
+      class="flex flex-col justify-center gap-5 rounded-xl bg-yellow-100 p-8 lg:col-start-1 lg:row-span-2 lg:row-start-1"
     >
       <h1 class="text-preset-3">
         Create and schedule content
-        <span class="text-purple-500 italic">quicker.</span>
+        <span class="italic text-purple-500">quicker.</span>
       </h1>
       <img
         src="/images/illustration-create-post.webp"
@@ -105,7 +105,7 @@
     </section>
 
     <section
-      class="lg:row-span-2 lg:col-start-1 lg:row-start-3 flex flex-col gap-5 justify-center p-8 bg-yellow-500 rounded-xl"
+      class="flex flex-col justify-center gap-5 rounded-xl bg-yellow-500 p-8 lg:col-start-1 lg:row-span-2 lg:row-start-3"
     >
       <h1 class="text-preset-3">Write your content using AI.</h1>
       <img
@@ -115,13 +115,13 @@
     </section>
 
     <section
-      class="md:col-span-2 lg:col-span-4 flex flex-col md:flex-row md:items-center text-center gap-5 justify-between p-8 bg-purple-500 rounded-xl text-white"
+      class="flex flex-col justify-between gap-5 rounded-xl bg-purple-500 p-8 text-center text-white md:col-span-2 md:flex-row md:items-center lg:col-span-4"
     >
       <h1 class="text-preset-4 text-sm">
         &copy; 2024
         <a
           href="mailto:zaharaa120@gmail.com"
-          class="text-yellow-500 rounded-xl hover:underline"
+          class="rounded-xl text-yellow-500 hover:underline"
           >Zahara Amalia</a
         >
         All rights reserved.
@@ -130,7 +130,7 @@
         Contact me at
         <a
           href="mailto:zaharaa120@gmail.com"
-          class="text-yellow-500 rounded-xl hover:underline"
+          class="rounded-xl text-yellow-500 hover:underline"
           >zaharaa120@gmail.com</a
         >
       </h1>
