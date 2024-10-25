f12d800a70b3f3838cb4564c6af4d1e81a2b65cd
zahara120 <zaharaa120@gmail.com>
2024-10-25 22:58:43 +0700

    feat: add google analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
   <html>
     <head>
+      <!-- Google tag (gtag.js) -->
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-TR7LBHJG4L"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-TR7LBHJG4L");
+      </script>
       <meta
         name="google-site-verification"
         content="Q7WWwneqokVhPXj3-uFaeoLjBPCGuWnyVSl1RSIpS1U"
