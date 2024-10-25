19534af15dc10bba8a7dc8a040ad2876a3c4fac7
zahara120 <zaharaa120@gmail.com>
2024-10-25 17:14:22 +0700

    feat: add about component

--- pages/index.vue
+++ pages/index.vue
@@ -77,57 +77,7 @@
       </a>
     </section>
 
-    <section
-      data-aos="fade-right"
-      data-aos-duration="1000"
-      class="relative row-start-2 flex flex-col items-center justify-center gap-5 overflow-hidden rounded-xl bg-purple-100 p-5 text-center md:col-span-2 lg:col-span-2 lg:col-start-4 lg:row-span-2 lg:row-start-1 lg:items-start lg:justify-start lg:text-start"
-    >
-      <h1 class="text-preset-3 lg:text-preset-2">Hi There, 👋🏻</h1>
-      <p class="lg:text-preset-4">
-        I'm a Web Developer with <mark>2 years</mark> of experience building
-        responsive and dynamic web applications. I specialize in creating
-        smooth, user-friendly interfaces using
-        <mark>Vue.js, React, and Tailwind CSS</mark>. Additionally, I am
-        proficient in <mark>Express.js</mark> for backend development.
-      </p>
-      <div class="hidden lg:block">
-        <img
-          src="/images/blob.png"
-          alt="blob"
-          class="absolute -bottom-5 -right-5 -z-10 w-[250px]"
-        />
-        <img
-          src="/images/pp1.png"
-          alt="photo profile"
-          class="absolute bottom-0 right-0 h-[240px] w-[220px] rounded-b-lg"
-        />
-        <img
-          src="/images/star.png"
-          alt="star"
-          class="absolute bottom-10 left-10 h-[50px] w-[50px]"
-        />
-        <img
-          src="/images/star.png"
-          alt="star"
-          class="absolute bottom-40 left-20 h-[30px] w-[30px]"
-        />
-        <img
-          src="/images/star2.png"
-          alt="star"
-          class="absolute bottom-40 right-5 h-[30px] w-[30px]"
-        />
-        <img
-          src="/images/star2.png"
-          alt="star"
-          class="absolute bottom-10 left-80 h-[50px] w-[50px]"
-        />
-      </div>
-      <img
-        src="/images/star.png"
-        alt="star"
-        class="absolute bottom-0 right-0 h-[150px] w-[150px] opacity-25 lg:hidden"
-      />
-    </section>
+    <About />
 
     <section
       data-aos="fade-left"
