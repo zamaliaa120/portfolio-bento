bf5f2eccb0cfd51d1423e26dbade4a9d04b7e085
zahara120 <zaharaa120@gmail.com>
2024-10-25 22:19:23 +0700

    feat: add SEO

--- app.vue
+++ app.vue
@@ -4,4 +4,25 @@
 
 <script setup>
 import "~/assets/styles/global.css";
+useHead({
+  title: "Portfolio | Zahara Amalia",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Explore the portfolio of Zahara Amalia, showcasing skills, projects, and achievements in web development.",
+    },
+    {
+      name: "keywords",
+      content: "Zahara Amalia, portfolio, web development, projects, skills",
+    },
+    { name: "author", content: "Zahara Amalia" },
+  ],
+  bodyAttrs: {
+    class: "portfolio-page",
+  },
+  script: [
+    { innerHTML: "console.log('Welcome to Zahara Amalia's Portfolio')" },
+  ],
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+definePageMeta({
+  title: "Zahara Amalia",
+  description: "Zahara Amalia's Portfolio",
+});
+</script>
+
 <template>
   <main
     class="grid min-h-screen grid-cols-1 justify-center gap-5 bg-[#F6F5F6] p-5 md:grid-cols-2 lg:grid-cols-4"
