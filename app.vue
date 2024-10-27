0d8ec639397c54cf4cc242549cc7cf5cec7d0ee6
zahara120 <zaharaa120@gmail.com>
2024-10-27 23:25:07 +0700

    feat: improve SEO

--- app.vue
+++ app.vue
@@ -6,7 +6,43 @@
 
 <script setup>
 import "~/assets/styles/global.css";
-import { useHead } from "#app";
+// useHead({
+//   title: "Portfolio | Zahara Amalia",
+//   meta: [
+//     {
+//       name: "description",
+//       content:
+//         "Explore the portfolio of Zahara Amalia, showcasing skills, projects, and achievements in web development.",
+//     },
+//     {
+//       name: "keywords",
+//       content: "Zahara Amalia, portfolio, web development, projects, skills",
+//     },
+//     { name: "author", content: "Zahara Amalia" },
+//   ],
+//   script: [
+//     {
+//       src: "https://www.googletagmanager.com/gtag/js?id=G-TR7LBHJG4L",
+//       async: true,
+//     },
+//     {
+//       innerHTML: `
+//         window.dataLayer = window.dataLayer || [];
+//         function gtag(){ dataLayer.push(arguments); }
+//         gtag('js', new Date());
+//         gtag('config', 'G-TR7LBHJG4L');
+//       `,
+//       type: "text/javascript",
+//     },
+//     {
+//       innerHTML: "console.log('Welcome to Zahara Amalia's Portfolio')",
+//       type: "text/javascript",
+//     },
+//   ],
+//   bodyAttrs: {
+//     class: "portfolio-page",
+//   },
+// });
 
 useHead({
   title: "Portfolio | Zahara Amalia",
@@ -21,28 +57,27 @@ useHead({
       content: "Zahara Amalia, portfolio, web development, projects, skills",
     },
     { name: "author", content: "Zahara Amalia" },
+    { name: "robots", content: "index, follow" },
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
   ],
   script: [
     {
       src: "https://www.googletagmanager.com/gtag/js?id=G-TR7LBHJG4L",
       async: true,
     },
-    {
-      innerHTML: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){ dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-TR7LBHJG4L');
-      `,
-      type: "text/javascript",
-    },
-    {
-      innerHTML: "console.log('Welcome to Zahara Amalia's Portfolio')",
-      type: "text/javascript",
-    },
   ],
   bodyAttrs: {
     class: "portfolio-page",
   },
 });
+
+if (process.client) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "G-TR7LBHJG4L");
+}
 </script>
